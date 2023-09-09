5d7446dc2500890a6544ee5383bd566689af42ea
ramiyoukhana7 <ramiprivatmail@gmail.com>
2023-09-10 00:14:57 +0200

    Lagt till så att man kan skriva kommentarer (Fortfarande mockup data)

--- src/components/PostPopup.jsx
+++ src/components/PostPopup.jsx
@@ -1,25 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./PostPopup.css";
 
+const useForceUpdate = () => {
+  const [value, setValue] = useState(0);
+  return () => setValue((value) => value + 1);
+};
+
 const PostPopup = ({ show, togglePopup, post }) => {
+  console.log(post);
+  const [newComment, setNewComment] = useState("");
+  const forceUpdate = useForceUpdate();
   if (!show) return null;
+
+  const handleComment = () => {
+    if (newComment.trim() === "") return;
+    const updatedComments = [...(post.comments || []), newComment];
+    post.comments = updatedComments;
+    setNewComment("");
+    forceUpdate();
+  };
 
   return (
     <div className="post-popup">
       <div className="post-popup-content">
-        <img src={post.image} alt="Post" className="popup-image" />
+        <img src={post.image} className="popup-image" />
         <div className="popup-details">
-          <img src={post.userImage} alt="User" className="popup-user-img" />
+          <img src={post.userImage} className="popup-user-img" />
           <h3 className="popup-username">{post.username}</h3>
           <p className="popup-description">{post.description}</p>
           <p className="popup-tags">{post.tags ? post.tags.join(" ") : ""}</p>
           <div className="popup-comments">
-            {post.comments
-              ? post.comments.map((comment, index) => (
-                  <p key={index}>{comment}</p>
-                ))
-              : "No comments yet."}
+            {post &&
+              post.comments &&
+              post.comments.map((comment, index) => (
+                <p key={index}>{comment}</p>
+              ))}
           </div>
+          <input
+            type="text"
+            placeholder="Add a comment"
+            value={newComment}
+            onChange={(e) => setNewComment(e.target.value)}
+          />
+          <button onClick={handleComment}>Post Comment</button>
           <button className="like-button">Like</button>
         </div>
       </div>
